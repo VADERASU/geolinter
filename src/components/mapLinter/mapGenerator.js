555aa5b3436f91a0f059d48b50f8e838d1f99251
Fan Lei <leifan833@msn.com>
2022-03-24 09:49:44 -0700

    unclassed map in case1

--- src/components/mapLinter/mapGenerator.js
+++ src/components/mapLinter/mapGenerator.js
@@ -26,10 +26,52 @@ class MapGenerator extends Component {
             });
 
         }else if(selectRawCase === 'state_education'){
+            /** SPATICL CASE 1 with un classed map */
+            let unclass_spec_string = 
+            `{
+                "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+                "width": 550,
+                "height": 300,
+                "background": "#F3F8FB",
+                "title": "The higher education rate of the states in the US",
+                "data": {
+                    "values": "state_education",
+                    "format": {
+                        "property": "features"
+                    }
+                },
+                "mark": "geoshape",
+                "projection": {
+                    "type": "equalEarth"
+                },
+                "encoding": {
+                    "stroke": {
+                        "value": "black"
+                    },
+                    "strokeWidth": {
+                        "value": 1
+                    },
+                    "color": {
+                        "field": "properties.higher_education_rate",
+                        "type": "quantitative",
+                        "legend": {
+                            "title": null
+                        }
+                    }         
+                },
+                "usermeta": {
+                    "embedOptions": {
+                        "actions": false
+                    }
+                }
+            }`;
+
+            let unclass_spec = JSON.parse(unclass_spec_string);
+            unclass_spec.data.values = selectedCaseData.geo;
             /** Preprocess the vega spec */
-            spec.data.values = selectedCaseData.geo;
+            //spec.data.values = selectedCaseData.geo;
             //spec.projection.fit = selectedCaseData.geo.features;
-            const result = embed(this.canvasRef.current, spec)
+            const result = embed(this.canvasRef.current, unclass_spec)
             .then((re)=>{
                 // result should be stored into the state
                 //console.log('Original Choropleth Map');
